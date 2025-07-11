ea3885f5d5ca255c1ae4df97179b639912c9a92b
CaHong0718 <sh55744@gmail.com>
2025-07-12 01:13:54 +0900

    Add Box

--- app/src/main/java/com/example/newcomposestudy/MainActivity.kt
+++ app/src/main/java/com/example/newcomposestudy/MainActivity.kt
@@ -4,13 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.example.newcomposestudy.ui.theme.NewComposeStudyTheme
 
 class MainActivity : ComponentActivity() {
@@ -38,10 +43,27 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     )
 }
 
-@Preview(showBackground = true)
+@Composable
+fun HelloBox() {
+    Box(
+        modifier = Modifier.fillMaxSize()
+            .background(Color(0xFF2196F3)),
+        contentAlignment = Alignment.Center
+    ) {
+        Text("Hello Compose!", fontSize = 24.sp)
+    }
+}
+
+/*@Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     NewComposeStudyTheme {
         Greeting("Android")
     }
+}*/
+
+@Preview(showBackground = true)
+@Composable
+fun HelloBoxPreview(){
+    HelloBox()
 }
